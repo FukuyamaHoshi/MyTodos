c2a61f378510b6993958d16cdc33ffcd8d91956b
福山帆士 <fukuyamahoshi@fukuyamahoshinoMacBook-Pro.local>
2021-03-08 22:03:00 +0900

    state method add

--- app/javascript/packs/form.jsx
+++ app/javascript/packs/form.jsx
@@ -2,16 +2,38 @@ import React from 'react'
 
 class Form extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: "",
+            lists: []
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log("submit");
+
+        this.setState({
+            lists: [
+                ...this.state.lists,
+                this.state.value
+            ]
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
     }
 
     render() {
         return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="やらなければいけないこと"/>
+            <div className="form-wrap">
+            <form onSubmit={this.handleSubmit} className="form">
+                <input type="text" placeholder="やらなければいけないこと" className="form-input" onChange={this.handleChange}/>
             </form>
             </div>
         )
